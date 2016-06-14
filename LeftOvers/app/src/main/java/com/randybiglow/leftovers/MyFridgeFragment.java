261e7ded690270c30926ad250b0174ae71926a31
Tianna N. McClanahan <tnmcclanahan@gmail.com>
2016-06-13 18:56:52 -0700

    starting listview

--- LeftOvers/app/src/main/java/com/randybiglow/leftovers/MyFridgeFragment.java
+++ LeftOvers/app/src/main/java/com/randybiglow/leftovers/MyFridgeFragment.java
@@ -6,14 +6,20 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by RandyBiglow on 6/13/16.
  */
 public class MyFridgeFragment extends Fragment{
+
+    ListView listView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_my_fridge, container, false);
+
+//        listView = (ListView)
     }
 }
