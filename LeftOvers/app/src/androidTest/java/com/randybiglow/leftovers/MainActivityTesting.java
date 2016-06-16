2a658c9922185ddbf6a12eb869401c857fc0fedb
Hang Tran <RandyBiglow@Randys-MacBook-Pro.local>
2016-06-16 16:57:49 -0700

    Test fab button.

--- LeftOvers/app/src/androidTest/java/com/randybiglow/leftovers/MainActivityTesting.java
+++ LeftOvers/app/src/androidTest/java/com/randybiglow/leftovers/MainActivityTesting.java
@@ -8,19 +8,32 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
-public class BetaTesting {
+public class MainActivityTesting {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
+    //Testing the fab button is clickable.
     @Test
-    public void iconAppearsFirst(){
-        onView(withId(R.id.fab)).perform(click()).check(matches(isClickable()));
+    public void fabButtonClicked(){
+        onView(withId(R.id.fab)).check(matches(isClickable()));
     }
+
+    //Testing the appearance of the text within the alert dialog.
+//    @Test
+//    public void textInDialog(){
+//        //onView(withText("Add New Item?")).check(matches(isDisplayed()));
+//        onView(withText(R.string.dialog_addnew)).inRoot(isDialog()).check(matches(isDisplayed()));
+//    }
+
+
+//    @Test
+//    public void nameEditText(){
+//        //onView(withId(R.id.name)).check(matches(isEnabled()));
+//    }
 }
