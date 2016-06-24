71288111dd1fd371cfc19b4740581183d281e843
Hang Tran <RandyBiglow@Randys-MacBook-Pro.local>
2016-06-24 12:33:11 -0700

    Webviews.

---
+++ LeftOvers/app/src/main/java/com/randybiglow/leftovers/WebView.java
@@ -0,0 +1,17 @@
+package com.randybiglow.leftovers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebView extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+
+    }
+}
